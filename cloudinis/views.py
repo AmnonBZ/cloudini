dcf1c79fa068ea30fadda34542d084ce258b2a0b
Amnon <amnon.bn1992@gmail.com>
2020-04-04 23:53:37 +0300

    tagenforcement policy

--- cloudinis/views.py
+++ cloudinis/views.py
@@ -1,30 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponseRedirect, HttpResponse
-from django.views.decorators.csrf import csrf_exempt
 from django.utils import timezone
-from .models import *
-import boto3
+from cloudinis.Policies.TagEnforcement import TagEnforecement
 
 
 # Create your views here.
 def index(request):
-    invalid = []
-    client = boto3.client('ec2', region_name="us-east-1")
-    response = client.describe_instances()
-    for i in response["Reservations"]:
-        try:
-            for j in i["Instances"][0]["Tags"]:
-                if not j["Key"] == "Owner":
-                    flag = 1
-                    break
-                else:
-                    flag = 0
-            if flag == 1:
-                invalid.append(i["Instances"][0]["PublicDnsName"])
-        except KeyError:
-            invalid.append(i["Instances"][0]["PublicDnsName"])
-    # invalid = ["1", "2", "3"]
-
+    invalid = TagEnforecement(1, 1)
     return render(request, 'main/index.html', {
         "invalid": invalid
     })
