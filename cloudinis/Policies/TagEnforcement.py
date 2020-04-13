d1af83d816ff79e9caf54a502da7bd905f7d3133
unknown <shmuelyos@cs.colman.ac.il>
2020-04-13 23:18:42 +0300

    added new policy -resourceType, and we need to fix tagenforcementS3

--- cloudinis/Policies/TagEnforcement.py
+++ cloudinis/Policies/TagEnforcement.py
@@ -3,7 +3,7 @@ import boto3
 
 # Currently - only applies to EC2
 
-def TagEnforecement(customer, policy):
+def TagEnforcement(customer, policy):
     # regionList = ["us-east-2", "us-east-1", "us-west-1", "us-west-2", "ap-east-1", "ap-south-1", "ap-northeast-3", "ap-northeast-2", "ap-southeast-1", "ap-southeast-2", "ap-northeast-1", "ca-central-1", "eu-central-1", "eu-west-1", "eu-west-2", "eu-west-3", "eu-north-1", "me-south-1", "sa-east-1"]
     regionList = ["us-east-1"]
 
@@ -11,7 +11,11 @@ def TagEnforecement(customer, policy):
 
     for region in regionList:
 
-        activated_policies = ActivatedPolicy.objects.get(customer=customer, policy=policy)
+        try:
+            activated_policies = ActivatedPolicy.objects.get(customer=customer, policy=policy)
+        except ActivatedPolicy.DoesNotExist:
+            activated_policies = None
+
 
         client = boto3.client('ec2', region_name=region)
         response = client.describe_instances()
--- cloudinis/Policies/TagEnforcementS3.py
+++ cloudinis/Policies/TagEnforcementS3.py
@@ -4,21 +4,28 @@ import boto3
 
 # Currently - only applies to EC2
 
+
 def TagEnforcementS3(customer, policy):
     # regionList = ["us-east-2", "us-east-1", "us-west-1", "us-west-2", "ap-east-1", "ap-south-1", "ap-northeast-3", "ap-northeast-2", "ap-southeast-1", "ap-southeast-2", "ap-northeast-1", "ca-central-1", "eu-central-1", "eu-west-1", "eu-west-2", "eu-west-3", "eu-north-1", "me-south-1", "sa-east-1"]
     regionList = ["us-east-1"]
+    invalid = []
+    bucketsName = []
 
-    arr = []
-    try:
-        for region in regionList:
+    for region in regionList:
+        try:
+            activated_policies = ActivatedPolicy.objects.get(customer=customer, policy=policy)
+        except ActivatedPolicy.DoesNotExist:
+            activated_policies = None
 
-        # activated_policies = ActivatedPolicy.objects.get(customer=customer, policy=policy)
+        client = boto3.client('s3', region_name=region)
+        response = client.list_buckets()
+        for i in response["Buckets"]:
+            try:
+                bucketsName.append(i["Name"])
+                if not set(activated_policies.metadata).issubset(set(bucketsName)):
+                    invalid.append(i["Name"])
 
-            client = boto3.client('s3', region_name=region)
-            response = client.list_buckets()
-            for i in response["Buckets"]:
-                arr.append(i["Name"])
-    except KeyError:
-        arr.append(i["Name"])
+            except KeyError:
+                invalid.append(i["Name"])
 
-    return arr
+    return invalid
