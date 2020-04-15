5d894bd2da3fb7bdc13462ebe3ca7cd399536d95
Amnon <amnon.bn1992@gmail.com>
2020-04-15 20:18:07 +0300

    authentication

---
+++ accounts/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            new_user = form.save()
+            new_user = authenticate(username=form.cleaned_data['username'],
+                                    password=form.cleaned_data['password1'],
+                                    )
+            login(request, new_user)
+            return redirect("/")
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/register.html', {'form': form})
+
--- cloudinis/models.py
+++ cloudinis/models.py
@@ -1,9 +1,39 @@
+from django.contrib.auth.models import User
 from django.db import models
 from django.contrib.postgres.fields import ArrayField
 
 
-class Customer(models.Model):
-    name = models.CharField(max_length=200)
+class Organization(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    main_user_password = models.CharField(max_length=32)
+
+    class Meta:
+        unique_together = ('name', 'email')
+
+    def save(self, *args, **kwargs):
+        super(Organization, self).save(*args, **kwargs)
+        CloudiniUser.objects.create(username=self.name, email=self.email, is_active=False,
+                                    password=self.main_user_password,
+                                    organization=Organization.objects.get(name=self.name))
+
+
+class CloudiniUser(models.Model):
+    username = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    organization = models.OneToOneField(Organization, on_delete=models.CASCADE)
+    password = models.CharField(max_length=32)
+    is_active = models.BooleanField(default=True)
+
+    class Meta:
+        unique_together = ('username', 'email')
+
+    def save(self, *args, **kwargs):
+        user = User.objects.create_user(username=self.username, password=self.password, email=self.email,
+                                        is_staff=False, is_superuser=False, is_active=self.is_active)
+        user.save()
+
+        super(CloudiniUser, self).save(*args, **kwargs)
 
 
 class Policy(models.Model):
@@ -12,7 +42,7 @@ class Policy(models.Model):
 
 
 class ActivatedPolicy(models.Model):
-    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    organization = models.ForeignKey(Organization, on_delete=models.CASCADE)
     policy = models.ForeignKey(Policy, on_delete=models.CASCADE)
     metadata = ArrayField(models.CharField(max_length=200))
     actionItem = models.CharField(max_length=200)
