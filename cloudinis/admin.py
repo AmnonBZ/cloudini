bee28d866d1717deeec198b1e190371ee1f5f6fe
Amnon <amnon.bn1992@gmail.com>
2020-04-22 23:50:38 +0300

    changes

--- cloudinis/admin.py
+++ cloudinis/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
+from django.contrib.auth.admin import UserAdmin
+from .models import *
 
-# Register your models here.
+admin.site.site_header = 'Cloudini Administration Panel'
+
+UserAdmin.list_display = ('username', 'organization', 'email', 'date_joined', 'is_active', 'is_staff')
+added_item = ('organization',)
+UserAdmin.list_filter = UserAdmin.list_filter + added_item
+
+class OrganizationDisplay(admin.ModelAdmin):
+    list_display = ('name', 'email')
+    list_filter = ('name', 'email')
+
+class PolicyDisplay(admin.ModelAdmin):
+    list_display = ('name', 'affectedResources')
+    list_filter = ('name', 'affectedResources')
+
+class ActivatedPolicyDisplay(admin.ModelAdmin):
+    list_display = ('organization', 'policy', 'metadata', 'actionItem')
+    list_filter = ('organization', 'policy', 'metadata', 'actionItem')
+
+class ViolationDisplay(admin.ModelAdmin):
+    list_display = ('connectedPolicy', 'resourceName')
+    list_filter = ('connectedPolicy', 'resourceName')
+
+admin.site.register(CloudiniUser, UserAdmin)
+admin.site.register(Organization, OrganizationDisplay)
+admin.site.register(Policy, PolicyDisplay)
+admin.site.register(ActivatedPolicy, ActivatedPolicyDisplay)
+admin.site.register(Violation, ViolationDisplay)
+
+admin.site.unregister(Group)
--- cloudinis/models.py
+++ cloudinis/models.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 from django.db import models
 from django.contrib.postgres.fields import ArrayField
+from django.contrib.auth.models import AbstractUser
 
 
 class Organization(models.Model):
@@ -13,32 +15,26 @@ class Organization(models.Model):
 
     def save(self, *args, **kwargs):
         super(Organization, self).save(*args, **kwargs)
-        CloudiniUser.objects.create(username=self.name, email=self.email, is_active=False,
+        applied_user_model = get_user_model()
+        applied_user_model.objects.create(username=self.name, email=self.email, is_active=False,
                                     password=self.main_user_password,
                                     organization=Organization.objects.get(name=self.name))
 
+    def __str__(self):
+        return self.name
 
-class CloudiniUser(models.Model):
-    username = models.CharField(max_length=100)
+
+class CloudiniUser(AbstractUser):
     email = models.EmailField(max_length=100)
-    organization = models.OneToOneField(Organization, on_delete=models.CASCADE)
-    password = models.CharField(max_length=32)
-    is_active = models.BooleanField(default=True)
-
-    class Meta:
-        unique_together = ('username', 'email')
-
-    def save(self, *args, **kwargs):
-        user = User.objects.create_user(username=self.username, password=self.password, email=self.email,
-                                        is_staff=False, is_superuser=False, is_active=self.is_active)
-        user.save()
-
-        super(CloudiniUser, self).save(*args, **kwargs)
+    organization = models.ForeignKey(Organization, on_delete=models.CASCADE, null=True)
 
 
 class Policy(models.Model):
     name = models.CharField(max_length=200)
     affectedResources = ArrayField(models.CharField(max_length=200))
+
+    def __str__(self):
+        return self.name
 
 
 class ActivatedPolicy(models.Model):
