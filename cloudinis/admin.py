4ab1ccd56fbe60cecfbefc5702e974c32d2e2889
Amnon <amnon.bn1992@gmail.com>
2020-04-24 13:57:57 +0300

    fix models

--- cloudinis/admin.py
+++ cloudinis/admin.py
@@ -5,13 +5,7 @@ from .models import *
 
 admin.site.site_header = 'Cloudini Administration Panel'
 
-UserAdmin.list_display = ('username', 'organization', 'email', 'date_joined', 'is_active', 'is_staff')
-added_item = ('organization',)
-UserAdmin.list_filter = UserAdmin.list_filter + added_item
-
-class OrganizationDisplay(admin.ModelAdmin):
-    list_display = ('name', 'email')
-    list_filter = ('name', 'email')
+UserAdmin.list_display = ('username', 'email', 'date_joined', 'is_active', 'is_staff')
 
 class PolicyDisplay(admin.ModelAdmin):
     list_display = ('name', 'affectedResources')
@@ -19,14 +13,13 @@ class PolicyDisplay(admin.ModelAdmin):
 
 class ActivatedPolicyDisplay(admin.ModelAdmin):
     list_display = ('organization', 'policy', 'metadata', 'actionItem')
-    list_filter = ('organization', 'policy', 'metadata', 'actionItem')
+    list_filter = ('organization', 'policy', 'actionItem')
 
 class ViolationDisplay(admin.ModelAdmin):
     list_display = ('connectedPolicy', 'resourceName')
     list_filter = ('connectedPolicy', 'resourceName')
 
 admin.site.register(CloudiniUser, UserAdmin)
-admin.site.register(Organization, OrganizationDisplay)
 admin.site.register(Policy, PolicyDisplay)
 admin.site.register(ActivatedPolicy, ActivatedPolicyDisplay)
 admin.site.register(Violation, ViolationDisplay)
--- cloudinis/models.py
+++ cloudinis/models.py
@@ -1,32 +1,23 @@
 from django.contrib.auth.models import User
-from django.contrib.auth import get_user_model
+# from django.contrib.auth import get_user_model
 from django.db import models
 from django.contrib.postgres.fields import ArrayField
 from django.contrib.auth.models import AbstractUser
-
-
-class Organization(models.Model):
-    name = models.CharField(max_length=100)
-    email = models.EmailField(max_length=100)
-    main_user_password = models.CharField(max_length=32)
-
-    class Meta:
-        unique_together = ('name', 'email')
-
-    def save(self, *args, **kwargs):
-        super(Organization, self).save(*args, **kwargs)
-        applied_user_model = get_user_model()
-        applied_user_model.objects.create(username=self.name, email=self.email, is_active=False,
-                                    password=self.main_user_password,
-                                    organization=Organization.objects.get(name=self.name))
-
-    def __str__(self):
-        return self.name
+from django.conf import settings
 
 
 class CloudiniUser(AbstractUser):
-    email = models.EmailField(max_length=100)
-    organization = models.ForeignKey(Organization, on_delete=models.CASCADE, null=True)
+    class Meta:
+        unique_together = ('username', 'email')
+
+    def save(self, *args, **kwargs):
+        if not self.is_superuser:
+            self.is_active = False
+        super(CloudiniUser, self).save(*args, **kwargs)
+
+
+    def __str__(self):
+        return self.username
 
 
 class Policy(models.Model):
@@ -38,12 +29,21 @@ class Policy(models.Model):
 
 
 class ActivatedPolicy(models.Model):
-    organization = models.ForeignKey(Organization, on_delete=models.CASCADE)
+    organization = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     policy = models.ForeignKey(Policy, on_delete=models.CASCADE)
+    affectedResource = models.CharField(max_length=200)
     metadata = ArrayField(models.CharField(max_length=200))
-    actionItem = models.CharField(max_length=200)
+    actionItem = ArrayField(models.CharField(max_length=200))
+    resourceTagToNotify = ArrayField(models.CharField(max_length=200))
+
+    def __str__(self):
+        return "{}-{}-{}".format(self.organization, self.policy, self.affectedResource)
 
 
 class Violation(models.Model):
     connectedPolicy = models.ForeignKey(ActivatedPolicy, on_delete=models.CASCADE)
     resourceName = models.CharField(max_length=200)
+    date = models.DateTimeField(auto_now_add=True) #TODO
+    isChecked = models.BooleanField(default=False)
+    isFixed = models.BooleanField(default=False)
+    fixedDate = models.DateTimeField(default=None, null=True)
