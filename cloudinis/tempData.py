46edd32f87631d33b773ebb26ca7c15ac38d2163
Michal Mittelman <michal@mittelman.co.il>
2020-04-25 17:54:06 +0300

    add frontend

---
+++ cloudinis/tempData.py
@@ -0,0 +1,79 @@
+
+policies = [
+    {
+        'name': 'policy1',
+        'title': 'first policy',
+        'action': 'all actions'
+    },
+
+    {
+        'name': 'policy2',
+        'title': 'second policy',
+        'action': 'all actions2'
+    },
+    {
+        'name': 'policy3',
+        'title': 'Third policy',
+        'action': 'all actions3'
+    }
+]
+
+
+profileData = [
+    {
+        'name': 'MyName',
+        'companyName': 'MyCompany',
+    }
+]
+
+policyType = ['EC2', 'S3', 'CloudWatch']
+
+EC2_policies =[
+    {
+        'name': 'policy1 EC2',
+        'title': 'first policy EC2',
+        'action': 'all actions EC2'
+    },
+
+    {
+        'name': 'policy2EC2',
+        'title': 'second policyEC2',
+        'action': 'all actions2EC2'
+    }
+]
+S3_policies =[
+    {
+        'name': 'policy1 S3',
+        'title': 'first policy S3',
+        'action': 'all actions S3'
+    },
+
+    {
+        'name': 'policy2 S3',
+        'title': 'second policy S3',
+        'action': 'all actions 2S3'
+    }
+]
+
+CloudWatch_policies =[
+    {
+        'name': 'policy1 CloudWatch',
+        'title': 'first policy CloudWatch',
+        'action': 'all actions CloudWatch'
+    },
+
+    {
+        'name': 'policy2 CloudWatch',
+        'title': 'second policy CloudWatch',
+        'action': 'all actions2 CloudWatch'
+    }
+]
+
+all_policies = {
+    'EC2': EC2_policies,
+    'S3': S3_policies,
+    'CloudWatch': CloudWatch_policies
+}
+
+
+
