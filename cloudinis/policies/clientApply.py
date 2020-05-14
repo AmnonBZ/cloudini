f5f342d079b88bcf87d2a37d6336976ce60b298e
unknown <shmuelyos@cs.colman.ac.il>
2020-05-14 15:25:21 +0300

    ...first...

--- cloudinis/policies/clientApply.py
+++ cloudinis/policies/clientApply.py
@@ -9,16 +9,10 @@ aws_secret_access_key="YBaxNzhQR1Gl5ZoOC7CEGRBh/Xho8dp46j54nRT2"
 aws_session_token="FwoGZXIvYXdzEDsaDJe6PNwQoQdY0J0LdiLDAb/5dFD0xgYfRv6HeLbE4PZwW5TXwjATfZCxQWqgwFVmYBqKBX1hTcpTqoa5AiiNQFjFWv+JamzO/TCaTI9eT4j22p/aiYz+BI5aN+ByBWt99PB5oWs2oHY07qlU9+h1md6AZGtlSoqYPDDJRg32Ns8iJ+/vUPRFbmNkOlkcuB4HQQs1pJtvpmvOdMPqupzP1WntTvCKWfxZkPqcOy8zO2jsq/ugS8vGk1Iwj6mYNtseTosJhljYQ2YKrs5/dQ2oIw7VZijupfT1BTItW8oJd32hgUkuO9bABRtjp5GXoaRlCMhAZqVqGFbvR0iJWeDlGkQJTyunfonD"
 
 def resourceHandler(resource, region):
-    return boto3.resource(resource, region_name=region,
-                        aws_access_key_id=aws_access_key_id,
-                          aws_secret_access_key=aws_secret_access_key,
-                            aws_session_token=aws_session_token)
+    return boto3.resource(resource, region_name=region,aws_access_key_id=aws_access_key_id,aws_secret_access_key=aws_secret_access_key,aws_session_token=aws_session_token)
 
 def clientHandler(resource, region):
-    return boto3.client(resource, region_name=region,
-                        aws_access_key_id=aws_access_key_id,
-                          aws_secret_access_key=aws_secret_access_key,
-                            aws_session_token=aws_session_token)
+    return boto3.client(resource, region_name=region,aws_access_key_id=aws_access_key_id,aws_secret_access_key=aws_secret_access_key,aws_session_token=aws_session_token)
 
 def clientApply(resource, wheretorun):
     regionList = ["us-east-1"]
